aec826cb0954d94fd8f50436ce7441e552c5037b
simpa007 <simpaimmey@gmail.com>
2023-10-19 13:42:36 -0700

    new update

--- app/component/home/Services.jsx
+++ app/component/home/Services.jsx
@@ -1,15 +1,11 @@
 import React from "react";
-import Image from "next/image";
 import { BsArrowUpRight } from "react-icons/bs";
-import web from "../../images/4.jpg";
-import seo from "../../images/1.jpg";
-import app from "../../images/5.jpg";
 
 export default function Services() {
 	return (
 		<div className="">
 			<div className="container mx-auto py-24">
-				<div className="border px-6 mx-auto md:flex md:justify-between md:items-center md:px-0">
+				<div className="px-6 mx-auto md:flex md:justify-between md:items-center md:px-0">
 					<div>
 						<p className="text-2xl font-extrabold mb-6">
 							<span className="text-[#6ff4a5]">/</span> SERVICES
@@ -32,61 +28,75 @@ export default function Services() {
 					</div>
 				</div>
 
-				<div className=" mt-12 mx-auto py-6 grid grid-cols-3 gap-4">
-					<div className="bg-[#2a313b] items-center max-w-lg mx-12 lg:mx-0">
-						<Image src={web} alt="web development" className="" />
-
-						<div className="p-6">
-							<h1>Web Development</h1>
-							<p className="mt-6">
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-								eiusmod tempor incididunt ut labore et dolore magna aliqua ut.
-							</p>
-							<button className="my-6 md:mt-8 underline underline-offset-8 text-lg font-bold">
-								View Details
-								<BsArrowUpRight
-									size={24}
-									className="inline-flex items-baseline"
-								/>
-							</button>
+				<div className=" mt-12 mx-auto py-6">
+					<div className="flex justify-between items-center">
+						<div className="w-1/2">
+							<span className="text-[#6ff4a5]">01</span>
+							<h1 className="text-3xl font-extrabold">Web Development</h1>
+						</div>
+						<div className="w-2/5">
+							<div className="flex space-x-8 items-end">
+								<p className="text-right">
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+									do eiusmod tempor incididunt ut labore et dolore magna aliqua
+									ut.
+								</p>
+								<span>
+									<BsArrowUpRight
+										size={45}
+										className="inline-flex items-baseline"
+									/>
+								</span>
+							</div>
 						</div>
 					</div>
-					<div className="bg-[#2a313b] items-center max-w-lg mx-12 lg:mx-0">
-						<Image src={seo} alt="seo" className="" />
-
-						<div className="p-6">
-							<h1>SEO</h1>
-							<p className="mt-6">
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-								eiusmod tempor incididunt ut labore et dolore magna aliqua ut.
-							</p>
-							<button className="my-6 md:mt-8 underline underline-offset-8 text-lg font-bold">
-								View Details
-								<BsArrowUpRight
-									size={24}
-									className="inline-flex items-baseline"
-								/>
-							</button>
+					<hr className="mt-4" />
+					<div className="mt-32 flex justify-between items-center">
+						<div className="w-1/2">
+							<span className="text-[#6ff4a5]">02</span>
+							<h1 className="text-3xl font-extrabold">
+								Search Engine Optimization (SEO)
+							</h1>
+						</div>
+						<div className="w-2/5">
+							<div className="flex space-x-8 items-end">
+								<p className="text-right">
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+									do eiusmod tempor incididunt ut labore et dolore magna aliqua
+									ut.
+								</p>
+								<span>
+									<BsArrowUpRight
+										size={45}
+										className="inline-flex items-baseline"
+									/>
+								</span>
+							</div>
 						</div>
 					</div>
-					<div className="bg-[#2a313b] items-center max-w-lg mx-12 lg:mx-0">
-						<Image src={app} alt="mobile development" className="" />
-
-						<div className="p-6">
-							<h1>App Development</h1>
-							<p className="mt-6">
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-								eiusmod tempor incididunt ut labore et dolore magna aliqua ut.
-							</p>
-							<button className="my-6 md:mt-8 underline underline-offset-8 text-lg font-bold">
-								View Details
-								<BsArrowUpRight
-									size={24}
-									className="inline-flex items-baseline"
-								/>
-							</button>
+					<hr className="mt-4" />
+					<div className="mt-32 flex justify-between items-center">
+						<div className="w-1/2">
+							<span className="text-[#6ff4a5]">03</span>
+							<h1 className="text-3xl font-extrabold">App Development</h1>
+						</div>
+						<div className="w-2/5">
+							<div className="flex space-x-8 items-end">
+								<p className="text-right">
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+									do eiusmod tempor incididunt ut labore et dolore magna aliqua
+									ut.
+								</p>
+								<span>
+									<BsArrowUpRight
+										size={45}
+										className="inline-flex items-baseline"
+									/>
+								</span>
+							</div>
 						</div>
 					</div>
+					<hr className="mt-4" />
 				</div>
 			</div>
 		</div>
--- app/component/home/Testimonial.jsx
+++ app/component/home/Testimonial.jsx
@@ -1,12 +1,13 @@
 "use client";
-
+import Image from "next/image";
 import { BsArrowUpRight } from "react-icons/bs";
+import picture from "../../images/5.jpg";
 
 function Testimonial() {
 	return (
 		<div className="bg-[#22282F]">
 			<div className="container mx-auto mt-28 py-24">
-				<div className="px-6 mx-auto md:flex md:justify-between md:items-center  md:px-0 border ">
+				<div className="px-6 mx-auto md:flex md:justify-between md:items-center md:px-0">
 					<div>
 						<p className="text-2xl font-extrabold mb-6">
 							<span className="text-[#6ff4a5]">/</span> TESTIMONIALS
@@ -28,8 +29,8 @@ function Testimonial() {
 					</div>
 				</div>
 
-				<div className=" mt-12 mx-auto py-6 grid grid-cols-1 gap-8 lg:gap-20 md:px-10 md:pb-10 lg:grid-cols-2">
-					<div className="flex flex-col items-center mx-12 lg:mx-0">
+				{/* <div className="mt-12 mx-auto py-6 grid grid-cols-1 gap-8 lg:gap-20 md:px-10 md:pb-10 lg:grid-cols-2"> */}
+				{/* <div className="flex flex-col items-center mx-12 lg:mx-0">
 						<div className="relative text-center">
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
@@ -58,8 +59,8 @@ function Testimonial() {
 						</div>
 						<span className="w-12 h-1 my-2 rounded-lg bg-red-400"></span>
 						<p>Leroy Jenkins</p>
-					</div>
-					<div className="flex flex-col items-center max-w-lg mx-12 lg:mx-0">
+					</div> */}
+				{/* <div className="flex flex-col items-center max-w-lg mx-12 lg:mx-0">
 						<div className="relative text-center">
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
@@ -98,8 +99,45 @@ function Testimonial() {
 						</div>
 						<span className="w-12 h-1 my-2 rounded-lg bg-blue-400"></span>
 						<p>Leroy Jenkins</p>
-					</div>
+					</div> */}
+				<div class="max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-16 lg:px-6">
+					<figure class="max-w-screen-md mx-auto">
+						<svg
+							class="h-12 mx-auto mb-3 text-gray-400 dark:text-gray-600"
+							viewBox="0 0 24 27"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z"
+								fill="currentColor"
+							/>
+						</svg>
+						<blockquote>
+							<p class="text-2xl font-medium">
+								"Flowbite is just awesome. It contains tons of predesigned
+								components and pages starting from login screen to complex
+								dashboard. Perfect choice for your next SaaS application."
+							</p>
+						</blockquote>
+						<figcaption class="flex items-center justify-center mt-6 space-x-3">
+							<Image
+								class="w-6 h-6 rounded-full"
+								src={picture}
+								alt="profile picture"
+							/>
+							<div class="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
+								<div class="pr-3 font-medium text-gray-200 dark:text-white">
+									Micheal Gough
+								</div>
+								<div class="pl-3 text-sm font-light text-gray-500 dark:text-gray-400">
+									Caninvest Properties
+								</div>
+							</div>
+						</figcaption>
+					</figure>
 				</div>
+				{/* </div> */}
 			</div>
 		</div>
 	);
